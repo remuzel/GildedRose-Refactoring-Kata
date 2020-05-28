6c0b30e33299e42753d2b5be4832feb03da08077
Remi <rku16@ic.ac.uk>
2020-05-28 21:12:59 +0200

    Refactoring into multiple factories -- TODO: Separate to individual files

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -1,3 +1,13 @@
+# For convenience (and because I always miss python)
+def max(*args)
+  args.max
+end
+
+def min(*args)
+  args.min
+end
+
+# Default guilded rose without the horrible ifs
 class GildedRose
 
   def initialize(items)
@@ -6,53 +16,11 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
-    end
+      ItemHandler.update(item)
   end
 end
 
+# Default items
 class Item
   attr_accessor :name, :sell_in, :quality
 
@@ -65,4 +33,129 @@ class Item
   def to_s()
     "#{@name}, #{@sell_in}, #{@quality}"
   end
+end
+
+# Setting the common fields and methods for all items
+class BaseItem
+  attr_reader :item
+
+  def initialize(item)
+    @item = item
+  end
+
+  def update
+    update_quality
+    update_sell_in
+  end
+
+  private
+
+  def update_quality
+  end
+
+  def update_sell_in
+  end
+end
+
+# The default behaviour for an items' change in quality and sell_in values
+class Default < BaseItem
+  private
+
+  def update_quality
+    quality = item.quality - (item.sell_in > 0 ? 1 : 2)
+
+    item.quality = max 0, quality
+  end
+
+  def update_sell_in
+    item.sell_in -= 1
+  end
+end
+
+# AgedBrie specific case
+class AgedBrie < BaseItem
+  private
+
+  def update_quality
+    quality += item.quality + (item.sell_in > 0 ? 1 : 2)
+
+    item.quality = min quality, 50
+  end
+
+  def update_sell_in
+    item.sell_in -= 1
+  end
+end
+
+# Sulfuras specific case (just the default)
+class Sulfuras < BaseItem
+end
+
+# Passes specific case
+class Passes < BaseItem
+  private
+
+  def update_quality
+    quality = if item.sell_in > 10
+                item.quality + 1
+              elsif item.sell_in > 5
+                item.quality + 2
+              elsif item.sell_in > 0
+                item.quality + 3
+              else
+                0
+              end
+
+    item.quality = min quality, 50
+  end
+
+  def update_sell_in
+    item.sell_in -= 1
+  end
+end
+
+# Cake specific case
+class Cake < BaseItem
+  private
+
+  def update_quality
+    quality = item.quality - 2
+    item.quality = max 0, quality
+  end
+
+  def update_sell_in
+    item.sell_in -= 1
+  end
+end
+
+# Wrapper for items, maps the unique name to the correct item handler
+class ItemHandler
+  attr_reader :item
+
+  MAP = {
+    'Aged Brie' => AgedBrie,
+    'Sulfuras, Hand of Ragnaros' => Sulfuras, 
+    'Backstage passes to a TAFKAL80ETC concert' => Passes,
+    'Conjured Mana Cake' => Cake
+  }
+
+  def initialize(item)
+    @item = item
+  end
+
+  def update
+    handler.new(item).update
+  end
+
+  def self.update(item)
+    new(item).update
+  end
+
+  private
+
+  def handler 
+    @handler ||= MAP[item.name] || Default 
+  end
+end
+
 end
